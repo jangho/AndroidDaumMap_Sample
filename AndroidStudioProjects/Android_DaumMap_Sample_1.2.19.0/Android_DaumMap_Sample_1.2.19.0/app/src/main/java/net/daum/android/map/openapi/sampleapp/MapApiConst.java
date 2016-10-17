ea6bd0f1d7bb5e2a22420dc75bb86a22e7bf08dd
Jangho <johnjanghochoi@gmail.com>
2016-10-17 18:27:27 +0900

    Build Android Daum Map sample

---
+++ AndroidStudioProjects/Android_DaumMap_Sample_1.2.19.0/Android_DaumMap_Sample_1.2.19.0/app/src/main/java/net/daum/android/map/openapi/sampleapp/MapApiConst.java
@@ -0,0 +1,9 @@
+package net.daum.android.map.openapi.sampleapp;
+
+/**
+ * Created by jan on 2016. 10. 17..
+ */
+public class MapApiConst {
+	// http://developers.daum.net/console
+    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "4145d72fc7da45c9eaff79c2c9862442";
+}
